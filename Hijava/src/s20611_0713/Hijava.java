4882b968fd3b40d2e0aa8328440c8e90c553fa90
BYE <BYE@LAPTOP-HG0J0BP0>
2021-07-13 13:47:00 +0900

    두번째 소스

--- Hijava/src/s20611_0713/Hijava.java
+++ Hijava/src/s20611_0713/Hijava.java
@@ -5,6 +5,8 @@ public class Hijava {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("첫번째 깃 소스");
+		
+		System.out.println("안녕, 소스 추가합니다.");
 	}
 
 }
